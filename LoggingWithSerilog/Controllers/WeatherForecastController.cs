405b52f7e8524e7cf7f5fc9e960de66cfef83405
Aram <aram@codingsonata.com>
2022-07-08 00:05:09 +0300

    Add project files.

---
+++ LoggingWithSerilog/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LoggingWithSerilog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            logger.LogDebug("Inside GetWeatherForecast endpoint");
+
+            throw new Exception("Failed to retrieve data");
+            var response = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+
+            logger.LogDebug($"The response for the get weather forecast is {JsonConvert.SerializeObject(response)}");
+            return response;
+        }
+    }
+}
